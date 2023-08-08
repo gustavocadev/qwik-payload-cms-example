bd41947ac76a7b3aa229038cea9a612d65ef28b9
gustavocadev <gustavocadev@gmail.com>
2023-08-08 11:44:55 -0500

    chore: show posts

--- app/src/routes/index.tsx
+++ app/src/routes/index.tsx
@@ -1,25 +1,41 @@
-import { component$ } from "@builder.io/qwik";
-import type { DocumentHead } from "@builder.io/qwik-city";
+import { component$ } from '@builder.io/qwik';
+import { routeLoader$, type DocumentHead, Link } from '@builder.io/qwik-city';
+import { getPosts } from '~/lib/server/payload.queries';
+
+export const usePosts = routeLoader$(() => {
+  return getPosts();
+});
 
 export default component$(() => {
+  const posts = usePosts();
+
   return (
-    <>
-      <h1>Hi 👋</h1>
-      <p>
-        Can't wait to see what you build with qwik!
-        <br />
-        Happy coding.
-      </p>
-    </>
+    <section class="px-4 xl:px-96 py-24">
+      <h2 class="text-3xl font-bold">Posts 📝</h2>
+      <ul>
+        {posts.value.docs.map((post) => {
+          return (
+            <li class="py-2" key={post.id}>
+              <Link
+                href={`/post/${post.id}/`}
+                class="text-blue-500 hover:underline"
+              >
+                {post.title}
+              </Link>
+            </li>
+          );
+        })}
+      </ul>
+    </section>
   );
 });
 
 export const head: DocumentHead = {
-  title: "Welcome to Qwik",
+  title: 'Welcome to Qwik',
   meta: [
     {
-      name: "description",
-      content: "Qwik site description",
+      name: 'description',
+      content: 'Qwik site description',
     },
   ],
 };
