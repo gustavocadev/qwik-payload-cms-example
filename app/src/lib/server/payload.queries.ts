34818e0ad1d3a6a232c0beb7c0106aaf938346bb
gustavocadev <gustavocadev@gmail.com>
2023-08-08 11:44:39 -0500

    chore: add queries

---
+++ app/src/lib/server/payload.queries.ts
@@ -0,0 +1,31 @@
+import type { PayloadCollection } from '~/types';
+import type { Post } from '@/payload/payload-types';
+import qs from 'qs';
+import ky from 'ky';
+
+const api = ky.create({
+  prefixUrl: `${process.env.PAYLOAD_PUBLIC_SERVER_URL}/api`,
+});
+
+export const getPosts = async () => {
+  const resp = await api.get('posts');
+  return resp.json<PayloadCollection<Post>>();
+};
+
+export const getPostById = async (id: string) => {
+  const query = {
+    id,
+  };
+
+  const stringifiedQuery = qs.stringify(
+    {
+      where: query,
+    },
+    {
+      addQueryPrefix: true,
+    }
+  );
+
+  const resp = await api.get(`posts/${stringifiedQuery}`);
+  return (await resp.json<PayloadCollection<Post>>()).docs[0];
+};
