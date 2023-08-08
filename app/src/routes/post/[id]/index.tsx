ea274b88e81190e48192a91300b506b606158016
gustavocadev <gustavocadev@gmail.com>
2023-08-08 12:59:39 -0500

    chore: use new path

--- app/src/routes/post/[id]/index.tsx
+++ app/src/routes/post/[id]/index.tsx
@@ -3,10 +3,7 @@ import { component$ } from '@builder.io/qwik';
 import { routeLoader$ } from '@builder.io/qwik-city';
 import { RichText } from '~/components/payload/RichText';
 import { getPostById } from '~/lib/server/payload.queries';
-import type {
-  FormattedElement,
-  FormattedText,
-} from '@/payload/rich-text-export';
+import type { FormattedElement, FormattedText } from '~/types/RichText';
 
 export const usePost = routeLoader$(({ params }) => {
   const id = params.id;
