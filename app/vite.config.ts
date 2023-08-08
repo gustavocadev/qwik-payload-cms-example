b6e12142b4e081d962ddb2b9089bd3d5db7dceed
gustavocadev <gustavocadev@gmail.com>
2023-08-08 11:44:02 -0500

    chore: disable click to source

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -1,14 +1,19 @@
-import { defineConfig } from "vite";
-import { qwikVite } from "@builder.io/qwik/optimizer";
-import { qwikCity } from "@builder.io/qwik-city/vite";
-import tsconfigPaths from "vite-tsconfig-paths";
+import { defineConfig } from 'vite';
+import { qwikVite } from '@builder.io/qwik/optimizer';
+import { qwikCity } from '@builder.io/qwik-city/vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig(() => {
   return {
-    plugins: [qwikCity(), qwikVite(), tsconfigPaths()],
+    plugins: [
+      qwikCity(),
+      // this isnt working yet in windows yet, only on macOS and linux
+      qwikVite({ devTools: { clickToSource: false } }),
+      tsconfigPaths(),
+    ],
     preview: {
       headers: {
-        "Cache-Control": "public, max-age=600",
+        'Cache-Control': 'public, max-age=600',
       },
     },
   };
